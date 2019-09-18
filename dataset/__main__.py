1dc7827eec7d7479b66ff012bb4e23f8e0c1a1e6
Vincent Wen <x.wen@student.tue.nl>
2019-09-18 23:56:57 +0200

    Rearrange the main function under dataset module.

--- dataset/__main__.py
+++ dataset/__main__.py
@@ -3,7 +3,7 @@ from dataset.utils import parse_dataset, create_vocabulary, QCDataset
 from preprocessing.utils import Embeddings
 from torch.utils.data import DataLoader
 import pickle
- 
+
 
 def main():
     """Example usage of the QCDataset and Embeddings classes.
@@ -11,17 +11,35 @@ def main():
     parser = argparse.ArgumentParser(description='Dataset commands.')
     parser.add_argument('--parse', default=False, type=bool, help='Parse original dataset into json.')
     args = parser.parse_args()
+
+    # Parse the dataset and generate the corresponding json files for 'train', 'val' and 'test'.
     if args.parse:
         parse_dataset()
+
     # Create an instance of the Embeddings class to load Gensim's Word2Vec word embeddings.
     embeddings = Embeddings()
+
     # Create the vocabulary given the train and val datasets and Word2Vec word embeddings.
     token2ind, ind2token = create_vocabulary(embeddings.model)
+
+    # Save the dictionaries for later use.
+    with open('dataset/ind2token', 'wb') as f:
+        pickle.dump(ind2token, f)
+
+    with open('dataset/token2ind', 'wb') as f:
+        pickle.dump(token2ind, f)
+
     # Create a truncated version of the Word2Vec word embeddings vector.
     embeddings_vector = embeddings.create_embeddings(token2ind)
     print(embeddings_vector.shape)
+
+    # Save the embeddings vector for later use.
+    with open('dataset/embeddings_vector', 'wb') as f:
+        pickle.dump(embeddings_vector, f)
+
     # Get rid of Gensim's Word2Vec word embedding to release the memory.
     del embeddings
+
     # Instantiate the QCDataset and DataLoader accordingly.
     qc_dataset = QCDataset(token2ind, ind2token, split='train', batch_first=False)
     qc_dataloader = DataLoader(qc_dataset, batch_size=16, collate_fn=qc_dataset.collate_fn, drop_last=True, pin_memory=True, shuffle=True)
@@ -31,18 +49,6 @@ def main():
         print(batch_inputs.size())
         print(batch_targets)
         break
-
-    embeddings_vector = embeddings.create_embeddings(token2ind)
-    print(embeddings_vector.shape)
-    with open('dataset/ind2token','wb') as f:
-        pickle.dump(ind2token,f) 
-        f.close()
-    with open('dataset/token2ind','wb') as f:
-        pickle.dump(token2ind,f) 
-        f.close()
-    with open('embeddings_vector','wb') as f:
-        pickle.dump(embeddings_vector,f) 
-    del embeddings
 
 
 if __name__ == '__main__':
