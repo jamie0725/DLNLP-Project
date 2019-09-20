ffcb06425820101ec20ae3472569f6ed85c99bfd
Robbie <luobutang@gmail.com>
2019-09-20 19:58:47 +0200

    Added validation and test on LSTM

--- LSTM/__main__.py
+++ LSTM/__main__.py
@@ -24,9 +24,8 @@ TEST_JSON_LOC = 'dataset/test/test.json'
 TRAIN_TXT_LOC = 'LSTM/results/train.txt'
 VAL_TXT_LOC = 'LSTM/results/val.txt'
 TEST_TXT_LOC = 'LSTM/results/test.txt'
-MODEL_LOC = 'LSTM/model/'
+MODEL_LOC = 'LSTM/model/best_model.pt'
 EMBEDDINGS_LOC = 'GoogleNews-vectors-negative300.bin'
-
 
 if __name__ == "__main__":
 
@@ -61,69 +60,93 @@ if __name__ == "__main__":
     train_data = load_json(TRAIN_JSON_LOC, label_map, name='Training Set')
     val_data = load_json(VAL_JSON_LOC, label_map, name='Validation Set')
     test_data = load_json(TEST_JSON_LOC, label_map, name='Test Set')
-
-    # Train model.
+    print_statement('LOAD EMBEDDINGS')
+    with open('dataset/ind2token', 'rb') as f:
+        ind2token = pickle.load(f)
+        f.close()
+    with open('dataset/token2ind', 'rb') as f:
+        token2ind = pickle.load(f)
+        f.close()
+    with open('dataset/embeddings_vector', 'rb') as f:
+        embeddings_vector = pickle.load(f)
+        f.close()
+    print_value('embeddings_shape', embeddings_vector.shape)
+    print_value('vocab_size', len(ind2token))
+    input_tensor, output_tensor = convert_to_tensor(train_data, label_map, token2ind)
+    embedding_length = embeddings_vector.shape[1]
+    qcdataset = QCDataset(token2ind, ind2token)
+    dataloader_train = DataLoader(qcdataset, batch_size=args.batch_size, shuffle=True, collate_fn=qcdataset.collate_fn)
+    qcdataset = QCDataset(token2ind, ind2token, split='val')
+    dataloader_validate = DataLoader(qcdataset, batch_size=args.batch_size, shuffle=True, collate_fn=qcdataset.collate_fn)
+    embeddings_vector_tensor = torch.from_numpy(embeddings_vector)
+    model = LSTMClassifier(output_size=len(label_map),
+                            hidden_size=args.num_hidden,
+                            embedding_length=embedding_length,
+                            embeddings_vector=embeddings_vector_tensor,
+                            lstm_layer=args.lstm_layer,
+                            lstm_dirc=args.lstm_bidirectional,
+                            trainable=args.embed_trainable,
+                            device=device)
+    model.to(device)
+     # Train model.
     if args.mode == 'train':
         # Model training.
-        print_statement('LOAD EMBEDDINGS')
-        with open('dataset/ind2token', 'rb') as f:
-            ind2token = pickle.load(f)
-            f.close()
-        with open('dataset/token2ind', 'rb') as f:
-            token2ind = pickle.load(f)
-            f.close()
-        with open('dataset/embeddings_vector', 'rb') as f:
-            embeddings_vector = pickle.load(f)
-            f.close()
-        print_value('embeddings_shape', embeddings_vector.shape)
-        print_value('vocab_size', len(ind2token))
-        input_tensor, output_tensor = convert_to_tensor(train_data, label_map, token2ind)
         print_statement('MODEL TRAINING')
-        embedding_length = embeddings_vector.shape[1]
-        qcdataset = QCDataset(token2ind, ind2token)
-        dataloader_train = DataLoader(qcdataset, batch_size=args.batch_size, shuffle=True, collate_fn=qcdataset.collate_fn)
-        qcdataset = QCDataset(token2ind, ind2token, split='val')
-        dataloader_validate = DataLoader(qcdataset, batch_size=args.batch_size, shuffle=True, collate_fn=qcdataset.collate_fn)
-        qcdataset = QCDataset(token2ind, ind2token, split='test')
-        dataloader_test = DataLoader(qcdataset, batch_size=args.batch_size, shuffle=True, collate_fn=qcdataset.collate_fn)
-        embeddings_vector_tensor = torch.from_numpy(embeddings_vector)
-        model = LSTMClassifier(output_size=len(label_map),
-                               hidden_size=args.num_hidden,
-                               embedding_length=embedding_length,
-                               embeddings_vector=embeddings_vector_tensor,
-                               lstm_layer=args.lstm_layer,
-                               lstm_dirc=args.lstm_bidirectional,
-                               trainable=args.embed_trainable,
-                               device=device)
-        model.to(device)
-        # with torch.no_grad():
-        #     model.embed.weight.data.copy_(torch.from_numpy(embeddings_vector))
-        #     model.embed.weight.requires_grad = False
         criterion = torch.nn.CrossEntropyLoss()
-        optim = torch.optim.Adam(model.parameters(), lr=args.lr)
-
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        best_eval = 0
         iteration = 0
         for i in range(args.epochs):
-            for step, (batch_inputs, batch_targets) in enumerate(dataloader_train):
+            for batch_inputs, batch_targets in dataloader_train:
                 iteration += 1
                 batch_inputs = batch_inputs.to(device)
                 batch_targets = batch_targets.to(device)
                 model.train()
-                optim.zero_grad()
+                optimizer.zero_grad()
                 output = model(batch_inputs)
                 loss = criterion(output, batch_targets)
                 accuracy = float(torch.sum(output.argmax(dim=1) == batch_targets)) / len(batch_targets)
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=args.max_norm)
-                optim.step()
+                optimizer.step()
                 if iteration % 10 == 0:
                     print('iter={:d}, loss={:4f}, acc={:.4f}'.format(iteration, loss, accuracy))
-                # if iteration % 50 ==0:
-                #     model.eval()
-                #     accs=[]
-                #     for i,(batch_inputs,batch_targets) in enumerate(dataloader_validate):
-                #         output =  model(batch_inputs)
-                #         acc = float(torch.sum(output.argmax(dim=1)== batch_targets)) / len(batch_targets)
-                #         accs.append(acc)
-                #     print(f'{step},{np.mean(accs)}')
-                #     model.train()
+                if iteration % 50 ==0 and iteration>0:
+                    model.eval()
+                    accs=[]
+                    for batch_inputs,batch_targets in dataloader_validate:
+                        with torch.no_grad():
+                            output =  model(batch_inputs)
+                        acc = float(torch.sum(output.argmax(dim=1)== batch_targets)) / len(batch_targets)
+                        accs.append(acc)
+                    validate_acc = np.mean(accs)
+                    print('===========================')
+                    print('validate_accuarcy={:.4f}'.format(validate_acc))
+                    # save best model parameters
+                    if validate_acc > best_eval:
+                        print("new highscore")
+                        best_eval = validate_acc
+                        ckpt = {
+                            "state_dict": model.state_dict(),
+                            "optimizerizer_state_dict": optimizer.state_dict(),
+                            "best_eval": best_eval,
+                        }
+                        torch.save(ckpt, MODEL_LOC)
+                    print('===========================')
+                    model.train()
+    else:
+        ckpt = torch.load(MODEL_LOC)
+        model.load_state_dict(ckpt["state_dict"])
+        print_statement('MODEL TESTING')
+        qcdataset = QCDataset(token2ind, ind2token, split='test')
+        dataloader_test = DataLoader(qcdataset, batch_size=args.batch_size, shuffle=True, collate_fn=qcdataset.collate_fn)
+        accs=[]
+        for batch_inputs,batch_targets in dataloader_validate:
+            with torch.no_grad():
+                output =  model(batch_inputs)
+            acc = float(torch.sum(output.argmax(dim=1)== batch_targets)) / len(batch_targets)
+            accs.append(acc)
+        test_acc = np.mean(accs)
+        print('Best model test_acc={:.4f}'.format(test_acc))
+        
+
