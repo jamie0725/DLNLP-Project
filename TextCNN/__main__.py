666b5bed4f91d8660eacfedc3a22e83a38a8de68
K. Liang <kai.liang@student.uva.nl>
2019-10-01 13:49:41 +0200

    enable TextCNN finetune

--- TextCNN/__main__.py
+++ TextCNN/__main__.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('--batch_size', type=int, default=64, help='number of examples to process in a batch')
     parser.add_argument('--num_classes', type=int, default=6, help='number of target classes')
     parser.add_argument('--max_norm', type=float, default=5.0, help='max norm of gradient')
-    parser.add_argument('--embed_trainable', type=bool, default=False, help='finetune pre-trained embeddings')
+    parser.add_argument('--embed_trainable', type=bool, default=True, help='finetune pre-trained embeddings')
     parser.add_argument('--kernel_sizes', nargs='+', type=int, default=[2, 3, 4], help='kernel sizes for the convolution layer')
     parser.add_argument('--device', type=str, default=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'))
     parser.add_argument('--p', type=float, default=0.5, help='dropout rate')
